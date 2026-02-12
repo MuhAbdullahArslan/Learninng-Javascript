7177bfa790a4f59cea99ddbabea82b719a17afeb
Muhammad Abdullah Arslan <abdullaharslan02024@gmail.com>
2026-02-12 04:00:28 +0000

    learning variable in js (w3school)

---
+++ 01_Basics/Variables/01_ variables.js
@@ -0,0 +1,80 @@
+
+// Variables = Data Containers
+// JavaScript variables are containers for data.
+
+// JavaScript variables can be declared in 4 ways:
+
+// Modern JavaScript:
+// Using let
+// Using const
+// Older JavaScript:
+// Using var (Not Recommended)
+// Automatically (Not Recommended)
+
+// Example using let
+// let x = 5;
+// let y = 6;
+// let z = x + y;
+// console.log(z)
+
+// Example using const
+// const x = 5;
+// const y = 6;
+// const z = x + y;
+// console.log(z)
+
+// Variables are labels for data values.
+// Variables are containers for storing data.
+
+// JavaScript Identifiers
+// Variables are identified with unique names called identifiers.
+
+// Names can be short like x, y, z.
+
+// Names can be descriptive age, sum, carName.
+
+// The rules for constructing names (identifiers) are:
+
+// Names can contain letters, digits, underscores, and dollar signs.
+// Names must begin with a letter, a $ sign or an underscore (_).
+// Names are case sensitive (X is different from x).
+// Reserved words (JavaScript keywords) cannot be used as names.
+
+// let _lastName = "Johnson";
+// let $myMoney = 5;
+
+// Declaring JavaScript Variables
+// Creating a variable in JavaScript is called declaring a variable.
+
+// You declare a JavaScript variable with the let keyword or the const keyword.
+
+// let userName;
+// console.log(userName);
+
+// userName = "Arslan"
+// console.log(userName);
+
+// Always use const if the value should not be changed
+// const carName = "Volvo";
+
+// const price1 = 5;
+// const price2 = 6;
+// let total = price1 + price2;
+
+// When to Use var, let, or const?
+// 1. Always declare variables
+
+// 2. Always use const if the value should not be changed
+
+// 3. Always use const if the type should not be changed (Arrays and Objects)
+
+// 4. Only use let if you cannot use const
+
+// 5. Never use var if you can use let or const.
+
+// One Statement, Many Variables
+// You can declare many variables in one statement.
+// Start the statement with let or constand separate the variables by comma:
+let person = "John Doe", carName = "Volvo", price = 200;
+
+
