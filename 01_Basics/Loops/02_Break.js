9bdb97a9e11553a35a1559348767fd3ab902d682
Muhammad Abdullah Arslan <abdullaharslan02024@gmail.com>
2026-02-12 13:42:05 +0000

    break in js

---
+++ 01_Basics/Loops/02_Break.js
@@ -0,0 +1,44 @@
+// Break in Loops
+// When break is encountered in a loop, the loop terminates immediately.
+
+// The program control is transfered to the statements following the loop.
+
+// No more loop iterations are executed.
+
+// let text = '';
+// for (let i = 0; i < 10;i++){
+//     if(i === 3){break;}
+//     text += "This number is: " + i + '\n';
+
+// }
+// console.log(text)
+
+// Labeled Break
+// Syntax
+// break labelname;
+// The break statement exits a loop or block and transfers the control to the labeled statement.
+
+// The break statement is particularly useful for breaking out of inner or outer loops from nested loops.
+
+// let text = '';
+
+// loop_outer : for(let j = 0; j < 5; j++){
+//     loop_inner : for(let i = 0; i < 5; i++ ){
+//         if(i === 3){ break loop_outer;}
+//         text += i + "\n"
+//     }  
+// }
+
+// console.log(text);
+
+
+let text = '';
+
+loop_outer : for(let j = 0; j < 5; j++){
+    loop_inner : for(let i = 0; i < 5; i++ ){
+        if(i === 3){ break loop_inner;}
+        text += i + "\n"
+    }  
+}
+
+console.log(text);
